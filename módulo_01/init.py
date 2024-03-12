725f8fe568154e0673b3e3d6f77107e8c79d30c4
Régis Lima <cawyn2@gmail.com>
2024-03-12 16:32:00 -0300

    entrendo em estrutura de dados

--- módulo_01/init.py
+++ módulo_01/init.py
@@ -1,33 +1,20 @@
+# Revisão da aula 01 do Curso de Formação em Análise de Dados em Python.
 import time
 
-print("Olá", end="aaaaa ")
-print("mundo!")
-
-# Revisão da aula 01 do Curso de Formação em Análise de Dados em Python.
-time.sleep(2)
-print('\n' * 100)
 print('Hello, World!')
 time.sleep(2)
-print('\n' * 100)
 print('Vamos fazer uma revisão de Python?')
 time.sleep(2)
-print('\n' * 100)
-print("Estamos usando a função 'print()' para exibir conteúdo na tela.")
+print("Estamos usando a função 'print()'")
+print("Usamos ela para exibir conteúdo na tela.")
 time.sleep(2)
-print('\n' * 100)
 print('Agora vamos armazenar o seu nome na variável nome.')
 time.sleep(2)
-print('\n' * 100)
 nome=input('Qual o seu nome? ')
-print('\n' * 100)
-print(f'Pronto, {nome}. Agora já temos seu nome armazenado')
-nomenew=type(nome)
 time.sleep(2)
-print('\n' * 100)
-print('o tipo de dado da variável nome é: ', nomenew)
+print(f'Pronto, {nome}. Agora já temos seu nome armazenado.')
 time.sleep(2)
-print('\n' * 100)
-type(nome)
+print('o tipo de dado da variável nome é: ', type(nome))
 # O método input() recebe uma entrada do usuário e armazena na variável nome. O valor armazenado é do tipo texto (string).
 # Caso seja seja inserido um valor numérico, o mesmo será convertido para texto.
 # Como solução é possível converter o tipo de dado antes de ser armazenado na variável.
