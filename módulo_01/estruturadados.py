5897f16b5d0d2d5c22d4f970e737e3b066caef3a
Régis Lima <cawyn2@gmail.com>
2024-03-18 11:05:11 -0300

    criei alguns exemplos de testes com listas

--- módulo_01/estruturadados.py
+++ módulo_01/estruturadados.py
@@ -1,1 +1,31 @@
-# As Principais Es
+# As Principais Estruturas de Dados em Python:
+# Listas
+lista=[] # cria uma lista vazia
+lista=['ana','costa','silva'] # cria uma lista com 3 elementos
+
+print(lista) # imprime a lista.
+print(len(lista)) # imprime o número de elementos da lista.
+## podemos usar operadores 'in' ou 'not in' para verificar se um elemento está ou não na lista
+
+listao=['antonio','jose','luiz',lista]
+res='ana' in listao # verifica se o elemento 'ana' está na lista e armazena o resultado na variável 'res'
+print(res) # imprime o conteudo da variável
+
+# Dicionários
+
+# Tuplas
+
+# Conjuntos
+
+
+
+
+
+
+# Pilhas
+
+# Filas
+
+# Arvores
+
+# Grafos
