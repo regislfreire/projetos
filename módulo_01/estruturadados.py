3d54c62a99c819e964407c421f72042b2c0c1b6a
Régis Lima <cawyn2@gmail.com>
2024-03-18 13:35:42 -0300

    Criei uma função e testei

--- módulo_01/estruturadados.py
+++ módulo_01/estruturadados.py
@@ -1,4 +1,9 @@
-cont=0
-while cont<10:
-    print(cont)
-    cont+=1
+#existem tres tipos de funçoes:
+#as definidas pelo programador:
+#as internas da linguagem:
+#as importadas das bibliotecas:
+def funcaoteste():
+    print('Exibindo uma mensagem por meio de uma função definida pelo programador')
+    return
+
+funcaoteste()
