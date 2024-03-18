21092768f03618068c534a1190d1edffd6516a15
Régis Lima <cawyn2@gmail.com>
2024-03-18 15:44:43 -0300

    apaguei as pastas

--- modulo_01/estruturadados.py
+++
@@ -1,9 +0,0 @@
-#existem tres tipos de funçoes:
-#as definidas pelo programador:
-#as internas da linguagem:
-#as importadas das bibliotecas:
-def funcaoteste():
-    print('Exibindo uma mensagem por meio de uma função definida pelo programador')
-    return
-
-funcaoteste()
--- modulo_02/init.py
+++
@@ -1,9 +0,0 @@
-# Modulo 02
-
-def teste():
-    print("Modulo 02")
-    print("Teste")
-    print("Teste2")
-    return 'ok'
-
-teste()
