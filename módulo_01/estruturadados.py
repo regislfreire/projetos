247b38784bf1262bb26b74ce1916276f879eea14
Régis Lima <cawyn2@gmail.com>
2024-03-18 13:25:38 -0300

    estrutura de repetição usando o While

--- módulo_01/estruturadados.py
+++ módulo_01/estruturadados.py
@@ -1,34 +1,4 @@
-# As Principais Estruturas de Dados em Python:
-# Listas
-lista=[] # cria uma lista vazia
-lista1=['ana','costa','silva'] # cria uma lista com 3 elementos
-
-print(lista1) # imprime a lista.
-#print(len(lista1)) # imprime o número de elementos da lista.
-## podemos usar operadores 'in' ou 'not in' para verificar se um elemento está ou não na lista
-
-lista2=['antonio','jose','luiz',lista1]
-print(lista2)
-for i in lista2:
-     print(i)
-#res='ana' in listao # verifica se o elemento 'ana' está na lista e armazena o resultado na variável 'res'
-#print(res) # imprime o conteudo da variável
-
-# Dicionários: são estruturas organizadas em pares (chave:valor)
-
-# Tuplas
-
-# Conjuntos
-
-
-
-
-
-
-# Pilhas
-
-# Filas
-
-# Arvores
-
-# Grafos
+cont=0
+while cont<10:
+    print(cont)
+    cont+=1
