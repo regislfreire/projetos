bbe90671f455327c92808fe8398789e8497055e6
regislfreire <cawynw2@gmail.com>
2024-07-23 08:32:18 -0300

    criei a estrutura básica pra aula.

---
+++ RPA/au.py
@@ -0,0 +1,5 @@
+import pyautogui
+import time
+import pandas as pd
+
+pyautogui.PAUSE = 2
--- RPA/index.py
+++
@@ -1,9 +0,0 @@
-'import pyautogui
-import time
-
-pyautogui.PAUSE = 1
-pyautogui.FAILSAFE = True
-
-pyautogui.moveTo(100, 100, duration=0.5)
-pyautogui.click()
-pyautogui.moveTo(200, 200, duration=0.5)
