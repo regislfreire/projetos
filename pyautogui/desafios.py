b8271fedb2e0af13b071aae5295a0d14b1681a82
regislfreire <cawynw2@gmail.com>
2024-07-31 16:41:19 -0300

    estou tendo problemas com acentos

--- pyautogui/desafios.py
+++ pyautogui/desafios.py
@@ -1,29 +1,28 @@
-# Pedi que o chatGPT criasse desafios para eu aprender a usar a biblioteca PyautoGUI. O nivel 0 são faceis e o nivel 5são dificeis.
-
 import pyautogui
 import pyperclip
 import time
+
+# Pedi que o chatGPT criasse desafios para eu aprender a usar a biblioteca PyautoGUI. O nivel 0 são faceis e o nivel 5são dificeis.
+pyautogui.FAILSAFE = True
 #pyautogui.PAUSE = 0.5 # Definindo que para casa de comando o PyAutoGUI irá aguardar 1 segundo entre cada comando
 
 # abrindo bloco de notas
-pyautogui.press('win', interval=0.5)
-pyautogui.write('notepad')
+time.sleep(1)
+pyautogui.press('win')
+pyautogui.write('notepad', interval=0.25)
 pyautogui.press('enter')
 
 # desafio 1, nivel 0 (mover o cursor para o canto superior esquerdo)
-pyautogui.moveTo(x=100, y=100, duration=2)
+#pyautogui.moveTo(x=100, y=100, duration=2)
 
 # desafio 2, nivel 1 (escrever o texto "Olá, PyAutoGUI")
-pyautogui.moveTo(500,420, duration=2)
-pyautogui.click()
-pyautogui.press('enter')
-pyautogui.write('Olá, PyAutoGUI'notepad
-                )
+pyautogui.moveTo(x=100, y=100, duration=2)
+pyautogui.write('Olá, PyAutoGUI', interval=0.5)
 #pyperclip.copy('Olá, PyAutoGUI')
 #pyautogui.hotkey('ctrl', 'v')
 
 # Fechando o Bloco de Notas
-time.sleep(10)
+time.sleep(5)
 pyautogui.hotkey('ctrl', 'a')
 pyautogui.press('del')
 pyautogui.hotkey('alt', 'f4')
